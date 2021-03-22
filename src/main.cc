5f4809d7dccbe4962f5db8ad7a3613ad8f4b075a
lother <563791534@qq.com>
2021-03-22 14:55:32 +0800

    feat : finish chap2

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,12 @@
+#include <algorithm>
 #include <iostream>
 
+#include "geometry.h"
 #include "tgaimage.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor blud = TGAColor(159, 216, 223, 255);
+const TGAColor blue = TGAColor(159, 216, 223, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
   bool steep = std::abs(y1 - y0) > std::abs(x1 - x0);
@@ -34,9 +36,34 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     error += 2 * dy;
   }
 }
+void triangle(Vec2i *points, TGAImage &image, TGAColor color) {
+  int max_x = std::max({points[0].u, points[1].u, points[2].u});
+  int min_x = std::min({points[0].u, points[1].u, points[2].u});
+  int max_y = std::max({points[0].v, points[1].v, points[2].v});
+  int min_y = std::min({points[0].v, points[1].v, points[2].v});
+  for (int x = min_x; x <= max_x; x++) {
+    for (int y = min_y; y < max_y; y++) {
+      Vec2i point = Vec2i(x, y);
+      Vec2i tri_line_0 = points[0] - points[1];
+      Vec2i tri_line_1 = points[1] - points[2];
+      Vec2i tri_line_2 = points[2] - points[0];
+      Vec2i point_line_0 = points[0] - point;
+      Vec2i point_line_1 = points[1] - point;
+      Vec2i point_line_2 = points[2] - point;
+      if (!(CrossProductSign(tri_line_0, point_line_0) ^
+            CrossProductSign(tri_line_1, point_line_1)) &&
+          !(CrossProductSign(tri_line_0, point_line_0) ^
+            CrossProductSign(tri_line_2, point_line_2))) {
+        image.set(x, y, color);
+      }
+    }
+  }
+}
 int main() {
-  TGAImage image(100, 100, TGAImage::RGB);
-  line(80, 40, 74, 13, image, blud);
+  TGAImage image(200, 200, TGAImage::RGB);
+  Vec2i pts[3] = {Vec2i(10, 10), Vec2i(100, 30), Vec2i(190, 160)};
+  triangle(pts, image, blue);
+
   image.flip_vertically();
   image.write_tga_file("output.tga");
   return 0;
